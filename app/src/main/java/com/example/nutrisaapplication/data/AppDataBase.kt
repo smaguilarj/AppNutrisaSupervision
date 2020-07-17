e1c642d9a880ace63aac0b8712439fb178e2e3ed
Antonio Acevedo <antonioacevedot@gmail.com>
2020-07-17 11:29:56 -0500

    commit nutrisa prueba app

--- app/src/main/java/com/example/nutrisaapplication/data/AppDataBase.kt
+++ app/src/main/java/com/example/nutrisaapplication/data/AppDataBase.kt
@@ -5,25 +5,25 @@ DELL
 17/07/2020
 NutrisaApplication
  */
-@Database(entities = [Developers::class], version = 1)
-abstract class AppDatabase : RoomDatabase() {
-    abstract fun developerDAO(): DeveloperDAO?
-
-    companion object {
-        private var INSTANCE: AppDatabase? = null
-        fun getAppDB(context: Context): AppDatabase? {
-            if (INSTANCE == null) {
-                INSTANCE = Room.databaseBuilder(
-                    context.getApplicationContext(),
-                    AppDatabase::class.java, Constantes.NAME_DATABASE
-                )
-                    .build()
-            }
-            return INSTANCE
-        }
-
-        fun destroyBD() {
-            INSTANCE = null
-        }
-    }
-}
+//@Database(entities = [Developers::class], version = 1)
+//abstract class AppDatabase : RoomDatabase() {
+//    abstract fun developerDAO(): DeveloperDAO?
+//
+//    companion object {
+//        private var INSTANCE: AppDatabase? = null
+//        fun getAppDB(context: Context): AppDatabase? {
+//            if (INSTANCE == null) {
+//                INSTANCE = Room.databaseBuilder(
+//                    context.getApplicationContext(),
+//                    AppDatabase::class.java, Constantes.NAME_DATABASE
+//                )
+//                    .build()
+//            }
+//            return INSTANCE
+//        }
+//
+//        fun destroyBD() {
+//            INSTANCE = null
+//        }
+//    }
+//}
